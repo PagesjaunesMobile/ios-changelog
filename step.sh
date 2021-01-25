e564b0fabf1f3084be46bd4396a93cb843a7811d
AmineArous <aminearous@live.fr>
2021-01-25 22:37:17 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -5,20 +5,21 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "*** Generate Changelog start ***"
 
 #Look for destination branch commit
-if [ -z "$BITRISEIO_GIT_BRANCH_DEST" ]; then
+#if [ -z "$BITRISEIO_GIT_BRANCH_DEST" ]; then
    lastLMasterTag=$(git rev-list --parents HEAD | head -1| cut -d' ' -f2)
-   else
-    git checkout ${BITRISEIO_GIT_BRANCH_DEST}~1
-    lastLMasterTag=$(git log --pretty=format:'%h' -n 1)
-fi
+ #  else
+  #  git checkout ${BITRISEIO_GIT_BRANCH_DEST}~1
+   # lastLMasterTag=$(git log --pretty=format:'%h' -n 1)
+#fi
 
 #Look for from branch commit
-git checkout ${BITRISE_GIT_BRANCH}
-lastLBranchTag=$(git log --pretty=format:'%h' -n 1)
-echo "BITRISE_GIT_BRANCH ${BITRISE_GIT_BRANCH}"
+#git checkout ${BITRISE_GIT_BRANCH}
+#lastLBranchTag=$(git log --pretty=format:'%h' -n 1)
+#echo "BITRISE_GIT_BRANCH ${BITRISE_GIT_BRANCH}"
 
+#git checkout HEAD
 #Generate changelog
-changelog="$(git log --pretty=format:"%s" $lastLMasterTag...$lastLBranchTag)"
+changelog="$(git log --pretty=format:"%s" $lastLMasterTag...HEAD)"
 
 #Save changelog in environment variable
 envman add --key CHANGELOG_TEXT --value "${changelog}"
